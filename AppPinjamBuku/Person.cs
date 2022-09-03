4f7171a5e3b703d3e85378bc11897d4516f6030f
Muhammad Ismail Azmi <88268092+mailbau@users.noreply.github.com>
2022-09-03 20:22:39 +0700

    Add files via upload

---
+++ AppPinjamBuku/Person.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppPinjamBuku
+{
+    internal class Person
+    {
+        public string Name { get; set; }
+        public string NIK { get; set; }
+
+        public class Member
+        {
+            public void pinjamBuku()
+            {
+                Book book = new Book();
+                Console.WriteLine("\nIsi keterangan berikut ini: ");
+                Console.WriteLine("\nNama anda: ");
+                string name = Console.ReadLine();
+                Console.WriteLine("Judul buku: ");
+                book.Title = Console.ReadLine();
+                Console.WriteLine("Penulis buku: ");
+                book.Author = Console.ReadLine();
+                Console.WriteLine("Tahun rilis buku: ");
+                book.ReleaseYear = Console.ReadLine();
+                Console.Clear();
+
+                Console.WriteLine("-----Keterangan peminjaman anda-----");
+                Console.WriteLine($"\nNama peminjam                 : {name}");
+                Console.WriteLine($"Judul buku yang dipinjam      : {book.Title}");
+                Console.WriteLine($"Penulis buku yang dipinjam    : {book.Author}");
+                Console.WriteLine($"Tahun rilis buku yang dipinjam: {book.ReleaseYear}");
+                Console.WriteLine("\nTerima kasih! Selamat datang kembali :)");
+            }
+            public void beliBuku()
+            {
+                Book book = new Book();
+                Console.WriteLine("\nIsi keterangan berikut ini: ");
+                Console.WriteLine("\nNama anda: ");
+                string name = Console.ReadLine();
+                Console.WriteLine("Judul buku: ");
+                book.Title = Console.ReadLine();
+                Console.WriteLine("Penulis buku: ");
+                book.Author = Console.ReadLine();
+                Console.WriteLine("Tahun rilis buku: ");
+                book.ReleaseYear = Console.ReadLine();
+                Console.WriteLine("Harga buku: ");
+                book.Price = Single.Parse(Console.ReadLine()); //Single.Parse() biar bisa nerima input berupa double/float
+                Console.Clear();
+
+                Console.WriteLine("-----Keterangan pembelian anda-----");
+                Console.WriteLine($"\nNama pembeli                : {name}");
+                Console.WriteLine($"Judul buku yang dibeli      : {book.Title}");
+                Console.WriteLine($"Penulis buku yang dibeli    : {book.Author}");
+                Console.WriteLine($"Tahun rilis buku yang dibeli: {book.ReleaseYear}");
+                Console.WriteLine($"Harga buku yang dibeli      : {book.Price}");
+                Console.WriteLine("\nTerima kasih! Selamat datang kembali :)");
+            }
+        }
+
+        public class NonMember
+        {
+            public void pinjamBuku()
+            {
+                Book book = new Book();
+                Console.WriteLine("\nIsi keterangan berikut ini: ");
+                Console.WriteLine("\nNama anda: ");
+                string name = Console.ReadLine();
+                Console.WriteLine("Judul buku: ");
+                book.Title = Console.ReadLine();
+                Console.WriteLine("Penulis buku: ");
+                book.Author = Console.ReadLine();
+                Console.WriteLine("Tahun rilis buku: ");
+                book.ReleaseYear = Console.ReadLine();
+                Console.Clear();
+
+                Console.WriteLine("-----Keterangan peminjaman anda-----");
+                Console.WriteLine($"\nNama peminjam                 : {name}");
+                Console.WriteLine($"Judul buku yang dipinjam      : {book.Title}");
+                Console.WriteLine($"Penulis buku yang dipinjam    : {book.Author}");
+                Console.WriteLine($"Tahun rilis buku yang dipinjam: {book.ReleaseYear}");
+                Console.WriteLine("\nTerima kasih! Selamat datang kembali :)");
+            }
+        }
+    }
+}
---
+++ AppPinjamBuku/Program.cs
@@ -0,0 +1,56 @@
+﻿using AppPinjamBuku;
+using System;
+
+namespace Program
+{
+    class Program
+    {
+        static void Main()
+        {
+            SelamatDatang();
+        }
+
+        static void SelamatDatang()
+        {
+            Console.WriteLine("-----Selamat datang di Perpustakaan Mail Ganteng!-----");
+            Console.WriteLine("\n-----Keuntungan menjadi member di Perpustakaan Mail Ganteng: -----");
+            Console.WriteLine("Bukan member: Hanya bisa meminjam buku. \nMember      : Bisa meminjam buku dan juga bisa membeli buku.");
+            Console.WriteLine("\nApakah kamu sudah menjadi member di Perpustakaan Mail Ganteng? (y/n)");
+
+            string member = Console.ReadLine();
+            Console.Clear();
+            Aksi(member);
+        }
+
+        static void Aksi(string member)
+        {
+            if (member == "y" | member == "Y")
+            {
+           
+                Person.Member myMember = new Person.Member();
+                Console.WriteLine("Apa yang ingin anda lakukan?");
+                Console.WriteLine("1. Pinjam buku \n2. Beli buku \nTulis angka dari yang ingin anda pilih.");
+                string pilih = Console.ReadLine();
+                if (pilih == "1")
+                {
+                    myMember.pinjamBuku();
+                }
+                else if (pilih == "2")
+                {
+                    myMember.beliBuku();
+                }
+            }
+            else if (member == "n" | member == "N")
+            {
+                Person.NonMember myMember = new Person.NonMember();
+                Console.WriteLine("Apa yang ingin anda lakukan?");
+                Console.WriteLine("1. Pinjam buku \nTulis angka dari yang ingin anda pilih.");
+                string pilih = Console.ReadLine();
+                if (pilih == "1")
+                {
+                    myMember.pinjamBuku();
+                }
+            }
+        }
+    }
+}
